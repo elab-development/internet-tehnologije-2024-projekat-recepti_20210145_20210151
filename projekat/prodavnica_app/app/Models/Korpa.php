5a8351c407c9ffa933d8dd6f3ace9197108a3694
marijailicc1 <marijailicc1@gmail.com>
2025-01-11 18:34:51 +0100

    Kreiran model korpa

---
+++ projekat/prodavnica_app/app/Models/Korpa.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+
+class Korpa extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['user_id', 'ukupna_cena', 'status'];
+
+    public function proizvodi()
+    {
+        return $this->belongsToMany(Proizvod::class, 'korpa_proizvod')
+                    ->withPivot('kolicina_proizvoda')
+                    ->withTimestamps();
+    }
+
+    public function korisnik()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}
